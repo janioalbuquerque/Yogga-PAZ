d5b6bc9486ebfc4f8542e206cf9bef61e7ebe736
Janio Albuquerque <aurelio.janio@gmail.com>
2020-07-21 09:24:22 -0300

    Navbar finalizada e adicionando font OpenSans

--- index.php
+++ index.php
@@ -7,8 +7,43 @@
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
 	<meta charset="utf-8" />
 	<meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=1.0">
+	<link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet">
 </head>
 <body>
+
+	<!--AQUI COMEÇA A NAVBAR-->
+
+		<nav class="navbar navbar-expand-lg navbar-dark">
+    <div class="d-flex flex-grow-1">
+        
+        <a class="navbar-brand ml-4" href="#">
+            <h4>Yogga<span class="text-dark">&PAZ</span></h4>
+        </a>
+        <div class="w-100 text-right">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#myNavbar7">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+        </div>
+    </div>
+    <div class="collapse navbar-collapse flex-grow-1 text-right" id="myNavbar7">
+        <ul class="navbar-nav ml-auto flex-nowrap">
+            <li class="nav-item">
+                <a href="#" class="nav-link text-light font-1 font-italic">Home</a>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link text-light font-1 font-italic">Serviços</a>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link text-light font-1 font-italic">Sobre</a>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link text-light font-1 font-italic">Contato</a>
+            </li>
+        </ul>
+    </div>
+</nav>
+
+	<!--AQUI TERMINA A NAVBAR-->
 
 
 
