91eaaa2c5fb094b4bfaa92997cabab1364e16b93
Janio Albuquerque <aurelio.janio@gmail.com>
2020-07-21 17:47:08 -0300

    Terminada aqui a primeira sessão(falta responsivo)!

--- index.php
+++ index.php
@@ -45,6 +45,60 @@
 
 	<!--AQUI TERMINA A NAVBAR-->
 
+	<!--AQUI COMEÇA A SESSÃO-1-->
+
+	<div class="container-fluid section-1">
+		
+		  <div class="row row-cols-2">
+		    <div class="col-6">
+		    	<div class="card  card-1 ">
+				  <div class="card-body">
+				    <h2>O QUE É YOGA?</h2>
+				    <p class="mt-3">Uma aula ou prática de yoga (como é comumente dito) não é uma pílula ou medicamento que se utiliza por certo tempo e depois do uso revela uma ‘cura’ ao praticante. Ela é similar às sessões de psicoterapia, pois, a prática de yoga ocorre de forma processual e à medida que vai se praticando (como uma caminhada no parque, hábito alimentar mais saudável), aprendendo as diversas posturas que o corpo é capaz de fazer (compreendendo as diversas formas possíveis de pensar sobre uma situação, na terapia), e formas de respirar (adquirindo novos comportamentos e posturas de acordo com a especificidade cognitiva) o praticante adquire uma nova forma de pensar e focar, adquirindo a capacidade de meditar não só na academia ou nos espaços (processo de alta da terapia, em que o terapeuta trabalhou com o paciente dele ser o seu próprio terapeuta), mas em qualquer lugar que queira meditar.</p>
+				  </div>
+				</div><!--card-1-->
+		    </div>
+
+		    <div class="col-6 card-2 ">
+		    	<div class="avatar-section1">
+		    		 <div class="card-body list ">
+				    <h3>Benefícios do Yoga?</h3>
+				    <ul>
+				    	<li>Previne dores e doenças</li>
+				    	<li>Reduz o estresse</li>
+				    	<li>Aumenta a flexibilidade</li>
+				    	<li>Emagrece e tonifica os músculos</li>
+				    	<li>Aumenta a flexibilidade</li>
+				    	<li>Ajuda a dormir melhor</li>
+				    	<li>Pode ser feito em qualquer lugar</li>
+				    </ul>
+				  </div>
+
+
+		    	<img class="" src="img/avatar-section1.png">
+		    	
+		    	</div><!--avatar-section1-->
+
+		    
+		    
+
+		    
+		  </div>
+
+		  </div>
+		    <div class="chamada">
+		    	<img src="img/icon-logo.svg">
+		    <h1>Yogga<span >&PAZ</span></h1>
+		    <h4 style="color: #ccc;">Cuidando da sua saúde mental e corporal.</h4 style="color: #ccc;">
+		    </div><!--chamada-->
+		    <div style="text-align: center; position: relative; bottom: -25px;"><svg width="3em" height="3em" viewBox="0 0 16 16" class="bi bi-arrow-down-circle-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+  <path fill-rule="evenodd" d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 5a.5.5 0 0 0-1 0v4.793L5.354 7.646a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 9.793V5z"/>
+</svg></div>
+		
+	</div>
+
+	<!--AQUI TERMINA A SESSÃO-1-->
+
 
 
 
@@ -52,6 +106,7 @@
 
 
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="js/script.js" type="text/javascript"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
 </body>
