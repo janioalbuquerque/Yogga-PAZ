bd790dca685512ba939012b2785f3a2324718fd1
Janio Albuquerque <aurelio.janio@gmail.com>
2020-07-29 17:55:41 -0300

    aplicado display flex no projeto e menu responsivo.

--- index.php
+++ index.php
@@ -2,68 +2,58 @@
 <html>
 <head>
 	<title>Yogga&PAZ</title>
-	<link href="https://fonts.googleapis.com/css?family=Oswald" rel="stylesheet">
-	<link href="css/style.css" rel="stylesheet">
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
-	<meta charset="utf-8" />
-	<meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=1.0">
-	<link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet">
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital@1&display=swap" rel="stylesheet">
 </head>
 <body>
 
 	<!--AQUI COMEÇA A NAVBAR-->
 
-		<nav class="navbar navbar-expand-lg navbar-dark">
-    <div class="d-flex flex-grow-1">
-        
-        <a class="navbar-brand ml-4" href="#">
-            <h4>Yogga<span class="text-dark">&PAZ</span></h4>
-        </a>
-        <div class="w-100 text-right">
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#myNavbar7">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-        </div>
-    </div>
-    <div class="collapse navbar-collapse flex-grow-1 text-right" id="myNavbar7">
-        <ul class="navbar-nav ml-auto flex-nowrap">
-            <li class="nav-item">
-                <a href="#" class="nav-link text-light font-1 font-italic">Home</a>
-            </li>
-            <li class="nav-item">
-                <a href="#" class="nav-link text-light font-1 font-italic">Serviços</a>
-            </li>
-            <li class="nav-item">
-                <a href="#" class="nav-link text-light font-1 font-italic">Sobre</a>
-            </li>
-            <li class="nav-item">
-                <a href="#" class="nav-link text-light font-1 font-italic">Contato</a>
-            </li>
-        </ul>
-    </div>
-</nav>
+	<input id="bt_menu" type="checkbox" name="">
+	<label for="bt_menu">&#9776;</label>
+
+	<nav class="menu">
+		
+		<a class="logo" href="">Yogga<span>&PAZZ</span></a>
+		
+		<ul>
+			<li><a href="">Home</a></li>
+			<li><a href="">Serviços</a>
+				<ul>
+					<li><a href="">Criação de Sites</a></li>
+					<li><a href="">Arte Visual</a></li>
+				</ul>
+			</li>
+			<li><a href="">Cursos</a>
+				<ul>
+					<li><a href="">Java</a></li>
+					<li><a href="">Photoshop</a></li>
+					<li><a href="">HTML/CSS</a></li>
+				</ul>
+			</li>
+			<li><a href="">Contatos</a></li>
+		</ul>
+	</nav>
 
 	<!--AQUI TERMINA A NAVBAR-->
 
-	<!--AQUI COMEÇA A SESSÃO-1-->
+	<!--AQUI COMEÇA A SESSÃO 1-->
 
-	<div class="container-fluid section-1">
-		
-		  <div class="row">
-		    <div class="col6 ">
-		    	<div class="card  card-1  ">
-				  <div class="card-body">
-				    <h2>O QUE É YOGA?</h2>
-				    <p class="mt-3">Uma aula ou prática de yoga (como é comumente dito) não é uma pílula ou medicamento que se utiliza por certo tempo e depois do uso revela uma ‘cura’ ao praticante. Ela é similar às sessões de psicoterapia, pois, a prática de yoga ocorre de forma processual e à medida que vai se praticando (como uma caminhada no parque, hábito alimentar mais saudável), aprendendo as diversas posturas que o corpo é capaz de fazer (compreendendo as diversas formas possíveis de pensar sobre uma situação, na terapia), e formas de respirar (adquirindo novos comportamentos e posturas de acordo com a especificidade cognitiva) o praticante adquire uma nova forma de pensar e focar, adquirindo a capacidade de meditar não só na academia ou nos espaços (processo de alta da terapia, em que o terapeuta trabalhou com o paciente dele ser o seu próprio terapeuta), mas em qualquer lugar que queira meditar.</p>
-				  </div>
-				</div><!--card-1-->
-		    </div>
+	<section class="section-1">
+		<div class="container">
+			<div class="item-container-1">
+				<div class="card">
+					 <h2>O QUE É YOGA?</h2>
+				    <p class="mt-3">Uma aula ou prática de yoga (como é comumente dito) não é uma pílula ou medicamento que se utiliza por certo tempo e depois do uso revela uma ‘cura’ ao praticante. Ela é similar às sessões de psicoterapia, pois, a prática de yoga ocorre de forma processual e à medida que vai se praticando (como uma caminhada no parque, hábito alimentar mais saudável), aprendendo as diversas posturas que o corpo é capaz de fazer.</p>
+				</div>
+				
+			</div><!--item-container-1-->
 
-		    <div class="col6">
-		    <div class=" card-2 ">
-		    	
-		    		 <div class="card-body list ">
-				    <h3>Benefícios do Yoga?</h3>
+			<div class="item-container-2">
+				<div class="card">
+					<img src="img/avatar-section1.png">
+					 <h2>Benefícios do Yoga?</h2>
 				    <ul>
 				    	<li>Previne dores e doenças</li>
 				    	<li>Reduz o estresse</li>
@@ -73,246 +63,33 @@
 				    	<li>Ajuda a dormir melhor</li>
 				    	<li>Pode ser feito em qualquer lugar</li>
 				    </ul>
-				  </div>
+				</div>
+				
+			</div><!--item-container-2-->
 
-				   
-
-		    	<img class="" src="img/avatar-section1.png">
-
-		    	
-		    	
-		    	 
-		    	</div>
-		    </div>
-		    
-
-		    
-		  
-
-		    
-		  </div>
-
-
-		    <div class="chamada">
+			<div class="chamada">
 		    	<img src="img/icon-logo.svg">
 		    <h1>Yogga<span >&PAZ</span></h1>
 		    <h4 style="color: #ccc;">Cuidando da sua saúde mental e corporal.</h4 style="color: #ccc;">
 		    </div><!--chamada-->
-		    
 
-		  </div>
-		    
-		
-	
+		</div><!--container-->
 
-	<!--AQUI TERMINA A SESSÃO-1-->
 
-	<!--AQUI COMEÇA A SESSÃO-2-->
-	
-	<div class="section-2">
-		<div class="fundo"></div><!--fundo-->
-
-		<div class="row">
-			
-			<div class="card" style="width: 25rem;">
-			  <img style="width: 12rem" src="img/card1.jpg" class="card-img-top" alt="...">
-			  <div class="card-body">
-			    <h5 class="card-title">Roberta, 30 anos, médica</h5>
-			    <p class="card-text">Comecei a prática de yoga na adolescência com auxílio do meu pai, mas só depois de conhecer a academia <strong>Yogga&PAZ</strong> que tive mais disciplina e senti efeitos duradouros. Hoje, quase dois anos depois, sinto-me mais tranquila, confiante e percebo que transmito essa energia para meu convívio, tanto familiar quanto profissional.</p>
-			   
-			  </div>
-			</div>
-
-			<div class="card" style="width: 25rem;">
-			  <img style="width: 12rem" src="img/card2.jpg" class="card-img-top" alt="...">
-			  <div class="card-body">
-			    <h5 class="card-title">Mari, 50 anos, Professora</h5>
-			    <p class="card-text">Com essa agitação do dia a dia o yoga é uma forma bastante eficiente para ter equilíbrio mental, físico e espiritual. Assim, quando percebi que não estava mais dando conta, busquei adotar o yoga na minha vida. Passei uma fase bastante difícil; agora, estou muito feliz por ter conseguido superar as minhas dificuldades e ter a possibilidade de saborear as minhas conquistas!</p>
-			   
-			  </div>
-			</div>
-
-			<div class="card" style="width: 25rem;">
-			  <img style="width: 12rem" src="img/card3.jpg" class="card-img-top" alt="...">
-			  <div class="card-body">
-			    <h5 class="card-title">Daniela, 40 anos, analista de sistemas</h5>
-			    <p class="card-text">Autoconhecimento para superar seus limites, aumento de flexibilidade, interiorizar e silenciar a mente são alguns dos benefícios colhidos da prática. Melhorar o seu mundo interno e externo é, sem dúvida, o melhor de tudo.</p>
-			    
-			  </div>
-			</div>
-			
-		</div><!--row-->
-
-		<div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
-		  <ol class="carousel-indicators">
-		    <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
-		    <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
-		    <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
-		  </ol>
-		  <div class="carousel-inner">
-		    <div class="carousel-item active">
-		      <img src="img/img-1.jpg" class="d-block" alt="...">
-		      <div class="carousel-caption d-none d-md-block">
-		        <h5>First slide label</h5>
-		        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-		      </div>
-		    </div>
-		    <div class="carousel-item">
-		      <img src="img/img-2.jpg" class="d-block" alt="...">
-		      <div class="carousel-caption d-none d-md-block">
-		        <h5>Second slide label</h5>
-		        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-		      </div>
-		    </div>
-		    <div class="carousel-item">
-		      <img src="img/img-3.jpg" class="d-block" alt="...">
-		      <div class="carousel-caption d-none d-md-block">
-		        <h5>Third slide label</h5>
-		        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-		      </div>
-		    </div>
-		    <div class="carousel-item">
-		      <img src="img/img-4.jpg" class="d-block" alt="...">
-		      <div class="carousel-caption d-none d-md-block">
-		        <h5>Third slide label</h5>
-		        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-		      </div>
-		    </div>
-		    <div class="carousel-item">
-		      <img src="img/img-5.jpg" class="d-block" alt="...">
-		      <div class="carousel-caption d-none d-md-block">
-		        <h5>Third slide label</h5>
-		        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-		      </div>
-		    </div>
-		    <div class="carousel-item">
-		      <img src="img/img-6.jpg" class="d-block" alt="...">
-		      <div class="carousel-caption d-none d-md-block">
-		        <h5>Third slide label</h5>
-		        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-		      </div>
-		    </div>
-		  </div>
-		  <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
-		    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-		    <span class="sr-only">Previous</span>
-		  </a>
-		  <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
-		    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-		    <span class="sr-only">Next</span>
-		  </a>
-		</div>
-
-		<form style="width: 40%; margin: 0 auto;">
-		  <div class="form-group">
-		    <h2 style="margin-bottom: 20px;">Envie-nos seu e-mail para saber mais!</h2>
-		    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
 		   
-		  </div>
-		 
-		  <button type="submit" class="btn  text-white bg-dark">Enviar <svg  width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-caret-right-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-  <path d="M12.14 8.753l-5.482 4.796c-.646.566-1.658.106-1.658-.753V3.204a1 1 0 0 1 1.659-.753l5.48 4.796a1 1 0 0 1 0 1.506z"/>
-</svg></button>
-		</form>
-	</div><!--section-2-->
+		    
 
-	<!--AQUI COMEÇA O FOOTER-->
+	</section><!--section-1-->
 
-		<!-- Footer -->
-<footer class="page-footer font-small blue pt-4">
 
-  <!-- Footer Links -->
-  <div class="container-fluid text-center text-md-left">
 
-    <!-- Grid row -->
-    <div class="row">
 
-      <!-- Grid column -->
-      <div class="col-md-4 mt-md-0 mt-3">
-
-        <!-- Content -->
-        <h5 class="text-uppercase">Yogga&PAZ</h5>
-        <p>is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting.</p>
-
-      </div>
-      <!-- Grid column -->
-
-      <hr class="clearfix w-100 d-md-none pb-3">
-
-      <!-- Grid column -->
-      <div class="col-md-4 mb-md-0 mb-3">
-
-        <!-- Links -->
-        <h5 class="text-uppercase">Links</h5>
-
-        <ul class="list-unstyled">
-          <li>
-            <a href="#!">Link 1</a>
-          </li>
-          <li>
-            <a href="#!">Link 2</a>
-          </li>
-          <li>
-            <a href="#!">Link 3</a>
-          </li>
-          <li>
-            <a href="#!">Link 4</a>
-          </li>
-        </ul>
-
-      </div>
-      <!-- Grid column -->
-
-      <!-- Grid column -->
-      <div class="col-md-3 mb-md-0 mb-3">
-
-        <!-- Links -->
-        <h5 class="text-uppercase">Links</h5>
-
-        <ul class="list-unstyled">
-          <li>
-            <a href="#!">Link 1</a>
-          </li>
-          <li>
-            <a href="#!">Link 2</a>
-          </li>
-          <li>
-            <a href="#!">Link 3</a>
-          </li>
-          <li>
-            <a href="#!">Link 4</a>
-          </li>
-        </ul>
-
-      </div>
-      <!-- Grid column -->
-
-    </div>
-    <!-- Grid row -->
-
-  </div>
-  <!-- Footer Links -->
-
-  <!-- Copyright -->
-  <div class="footer-copyright text-center py-3">© 2020 Copyright:
-    <a href="https://mdbootstrap.com/"> Janio Albuquerque</a>
-  </div>
-  <!-- Copyright -->
-
-</footer>
-<!-- Footer -->
-
-	<!--AQUI TERMINA O FOOTER-->
+	<!--AQUI TERMINA A SESSÃO 1-->
 
 
 
 
 
 
-
-<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-<script src="js/script.js" type="text/javascript"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
 </body>
 </html>
